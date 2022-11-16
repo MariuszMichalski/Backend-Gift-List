d21e816d6e504010aef12d137a4b68dfc28ec29a
Mariusz <mario.m4@interia.pl>
2022-11-16 14:12:58 +0100

    Changed red.render into res.json and added CORS to index.js

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as cors from 'cors';
 import "express-async-errors";
 import {homeRouter} from "./routers/home";
 import {childRouter} from "./routers/child";
@@ -13,8 +14,9 @@ app.use(express.urlencoded({
 }));
 app.use(express.static('public'));
 app.use(express.json()); // Content-type: application/json
-
-
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.use('/', homeRouter);
 app.use('/child', childRouter);
